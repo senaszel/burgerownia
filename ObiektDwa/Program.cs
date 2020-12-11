3cdcfb9d0ab7c22c50ef295934a0daf088ca3f93
Marek Stanisław Meyer <marekstanislawmeyer@gmail.com>
2020-12-11 18:56:39 +0100

    some db sqlite work done

--- ObiektDwa/Program.cs
+++ ObiektDwa/Program.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ObiektDwa
+﻿namespace ObiektDwa
 {
     class Program
     {
         static void Main()
         {
-            //var exampleNEW_WAY = new Burger("here", new Ingredients(1, 2, 3).GetAll());
-            //var exampleOLDway = new Burger("Vege Burger", new List<Ingredient>() { new Ingredient("vegeMeatsubstitute", 9.50) });
+            //MenuManager menu = new MenuManager();
+            //menu.Welcome();
+            //menu.BeginingOfService(false);
 
-            //Console.WriteLine(exampleNEW_WAY);
-            //Console.WriteLine(exampleOLDway);
-            //Console.WriteLine("ok");
-
-            MenuManager menu = new MenuManager();
-            menu.Welcome();
-            menu.BeginingOfService(false);
+            burgerownia.BazaDanych_SQLite.SQLite_DB db = new burgerownia.BazaDanych_SQLite.SQLite_DB();
         }
     }
 }
---
+++ burgerownia.BazaDanych_SQLite/SQLite_DB.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace burgerownia.BazaDanych_SQLite
+{
+    public class SQLite_DB
+    {
+        SqliteConnectionStringBuilder connectionStringBuilder;
+        public SQLite_DB()
+        {
+            connectionStringBuilder = new SqliteConnectionStringBuilder();
+            //Use DB in project directory.  If it does not exist, create it:
+            connectionStringBuilder.DataSource = "./SqliteDB.db";
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                //Create a table (drop if already exists first):
+
+                var delTableCmd = connection.CreateCommand();
+                delTableCmd.CommandText = "DROP TABLE IF EXISTS Ingredients";
+                delTableCmd.ExecuteNonQuery();
+
+                var createTableCmd = connection.CreateCommand();
+                createTableCmd.CommandText = "CREATE TABLE Ingredients (id INTEGER PRIMARY KEY AUTOINCREMENT ,name VARCHAR(50) NOT NULL, price INTEGER NOT NULL)";
+                createTableCmd.ExecuteNonQuery();
+
+                //Seed some data:
+                SeedData("1, \'miesko\', 1000");
+                SeedData("2, \'serek\', 500");
+
+                //Read the newly inserted data:
+                ReadData();
+
+            }
+        }
+
+        private void ReadData()
+        {
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                var selectCmd = connection.CreateCommand();
+                selectCmd.CommandText = "SELECT * FROM Ingredients";
+
+                using (var reader = selectCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var message = reader.GetString(0);
+                        var message2 = reader.GetString(1);
+                        var message3 = reader.GetString(2);
+                        Console.Write(message);
+                        Console.Write(message2);
+                        Console.WriteLine(message3);
+                    }
+                }
+            }
+        }
+
+        private void SeedData(string values)
+        {
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var insertCmd = connection.CreateCommand();
+
+                    insertCmd.CommandText = $"INSERT INTO Ingredients VALUES({values})";
+                    insertCmd.ExecuteNonQuery();
+
+
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
+
